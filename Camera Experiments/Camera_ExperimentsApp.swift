30c559ce82bf52cf1ac11355c0f92a6abd6b6d47
Josh Levine <josh.levine1@gmail.com>
2022-01-23 15:51:00 -0800

    Initial Commit

---
+++ Camera Experiments/Camera_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_ExperimentsApp.swift
+//  Camera Experiments
+//
+//  Created by Joshua Levine on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Camera_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
